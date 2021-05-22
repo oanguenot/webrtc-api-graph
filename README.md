0c9d7e7e489b3989f7083a6bebbefb319bc5a70e
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2021-05-22 17:59:18 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # webrtc-api-graph
 Display a graph representing the WebRTC JavaScript Object models and relation.
+
+## Install
+
+Serve 2 files `webrtc-api.html` and `webrtc-api.js` to your server (node.js or any).
+
+## Usage
+
+Once loaded, the page will inspect the WebRTC API implemented by the browser used.
+
+Click on the button **Save** to export an image of the model.
+
+## Early-version
+
+This is a dirty and early version. Don't hesitate to propose enhancements.
---
+++ webrtc-api.js
@@ -0,0 +1,127 @@
+function saveSvg(svgEl, name) {
+  svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  var svgData = svgEl.outerHTML;
+  var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+  var svgBlob = new Blob([preface, svgData], { type: "image/svg+xml;charset=utf-8" });
+  var svgUrl = URL.createObjectURL(svgBlob);
+  var downloadLink = document.createElement("a");
+  downloadLink.href = svgUrl;
+  downloadLink.download = name;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+const parseUA = (userAgent) => {
+  if (userAgent.includes('Firefox/')) {
+    return (`Firefox-v${userAgent.split('Firefox/')[1].split(" ")[0]}`)
+  }
+
+  if (userAgent.includes('Edg/')) {
+    return (`Edge-v${userAgent.split('Edg/')[1].split(" ")[0]}`)
+  }
+
+  if (userAgent.includes('Chrome/')) {
+    return (`Chrome-v${userAgent.split('Chrome/')[1].split(" ")[0]}`)
+  }
+
+  if (userAgent.includes('Safari/')) {
+    return (`Safari-v${userAgent.split('Safari/')[1].split(" ")[0]}`)
+  }
+}
+
+const ready = async () => {
+  console.log('DOM is ready');
+  const button = document.querySelector('#save');
+  const browser = document.querySelector('#browser');
+  const browser_name = parseUA(navigator.userAgent);
+  browser.innerHTML = browser_name
+
+  button.onclick = async () => {
+    const svg = document.querySelector("#render")
+    saveSvg(svg, `webrtc_model_${browser_name}.svg`);
+  }
+
+  mermaid.initialize({ startOnLoad: true });
+
+  let model = "classDiagram\n"
+
+  const webrtcAPI = [
+    "MediaDevices",
+    "MediaDeviceInfo",
+    "MediaStream",
+    "MediaStreamTrack",
+    "RTCPeerConnection",
+    "RTCDataChannel",
+    "RTCCertificate",
+    "RTCDTMFSender",
+    "RTCDtlsTransport",
+    "RTCIceCandidate",
+    "RTCIceTransport",
+    "RTCRtpTransceiver",
+    "RTCRtpReceiver",
+    "RTCRtpSender",
+    "RTCSctpTransport",
+    "RTCSessionDescription",
+    "RTCStatsReport",
+  ]
+  // Relation
+  model += "MediaStreamTrack <-- RTCPeerConnection\n"
+  model += "MediaStreamTrack <-- MediaStream\n"
+  model += "MediaStream <-- RTCPeerConnection\n"
+  model += "MediaDeviceInfo <-- MediaDevices\n"
+  model += "MediaStream <-- MediaDevices\n"
+  model += "RTCRtpTransceiver <-- RTCPeerConnection\n"
+  model += "RTCRtpReceiver <-- RTCRtpTransceiver\n"
+  model += "RTCRtpSender <-- RTCRtpTransceiver\n"
+  model += "MediaStreamTrack <-- RTCRtpSender\n"
+  model += "MediaStreamTrack <-- RTCRtpReceiver\n"
+  model += "RTCDtlsTransport <-- RTCRtpSender\n"
+  model += "RTCDTMFSender <-- RTCRtpSender\n"
+  model += "RTCDtlsTransport <-- RTCRtpReceiver\n"
+  model += "RTCIceTransport <-- RTCDtlsTransport\n"
+  model += "RTCIceCandidate <-- RTCIceTransport\n"
+  model += "RTCStatsReport <-- RTCRtpReceiver\n"
+  model += "RTCStatsReport <-- RTCRtpSender\n"
+  model += "RTCStatsReport <-- RTCPeerConnection\n"
+  model += "RTCDataChannel <-- RTCPeerConnection\n"
+  model += "RTCSessionDescription <-- RTCPeerConnection\n"
+  model += "RTCCertificate <-- RTCPeerConnection\n"
+  model += "MediaStream <-- RTCRtpSender\n"
+
+  // Discover API
+  webrtcAPI.forEach(apiName => {
+
+    try {
+      const api = eval(apiName)
+      if (api) {
+        console.log(">>>", api)
+        model += `class ${api.name}{\n`
+        const descriptors = Object.getOwnPropertyDescriptors(api.prototype)
+
+        Object.keys(descriptors).sort((a, b) => (a.localeCompare(b))).forEach(key => {
+          if (key !== "constructor") {
+            const obj = descriptors[key]
+            if ("value" in obj) {
+              model += `${key}()\n`
+            } else {
+              model += `${key}\n`
+            }
+          }
+        });
+        model += "}\n";
+      }
+    } catch (err) {
+      console.log(">>>err", err);
+    }
+  })
+
+  var element = document.querySelector("#webrtc");
+
+  var insertSvg = function (svgCode, bindFunctions) {
+    element.innerHTML = svgCode;
+  };
+  var graph = mermaid.mermaidAPI.render('render', model, insertSvg);
+}
+
+document.addEventListener("DOMContentLoaded", ready);
